1291e92d2b6439238228ab9fa456df23ce19e6a1
gabrieval <gabriela.valenzuela.rrii@gmail.com>
2024-03-24 20:01:07 -0300

    Heroe By Id

--- src/components/heroes/HeroeScreen.js
+++ src/components/heroes/HeroeScreen.js
@@ -1,10 +1,51 @@
 import React from 'react'
+import { Navigate, useParams } from 'react-router-dom'
+import { getHeroById } from '../../selectors/getHeroById';
 
 export const HeroeScreen = () => {
-  return (
-    <div>
+  const {heroeId} = useParams();
+ const hero = getHeroById ( heroeId );
 
-        <h1>HeroeScreen</h1>
+ if  ( !hero ){
+  return <Navigate to =" / "/>;
+ }
+const {
+        
+        superhero,
+        publisher,
+        alter_ego,
+        first_appearance,
+        characters,
+}= hero;
+
+  return (
+    <div className=" row mt-5">
+      <div className="col-4">
+        <img src={`../../../assets/heroes/${ heroeId  }.jpg`}
+        alt={ superhero }
+        className="img-thumbnail"
+        /> 
+     
+      </div>
+      <div className="col-8">
+        <h3>{ superhero }</h3>
+        <ul className="list-group list-group-flush">
+
+          <li className=" list-group-item">  <b>Alter ego: </b>   {alter_ego} </li>
+          <li className=" list-group-item">  <b>Publisher: </b>   {publisher} </li>
+          <li className=" list-group-item">  <b>First appearance: </b>   {first_appearance} </li>
+        </ul>
+
+        <h5> characters </h5>
+        <p> { characters }</p>
+        <button className="btn btn-outline-info">
+          Return
+        </button>
+
+
+      </div>
+
+       
     </div>
   )
 }
