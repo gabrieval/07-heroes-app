15b9dda0f45630045e718b5cb486ab8ae0d2461d
gabrieval <gabriela.valenzuela.rrii@gmail.com>
2024-03-25 20:17:56 -0300

    Aplicar Filtro

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,39 +1,47 @@
 import React from "react";
+
+import queryString from 'query-string';
 import { heroes } from "../../data/heroes";
 import { HeroCard } from "../heroes/HeroCard";
 import { useForm } from "../../hooks/useForm";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export const SearchScreen = () => {
-  const heroesFiltered = heroes;
+const location = useLocation ();
 
-  
-  const [ formValues, handleInputChange ] = useForm ( {
-    searchText:''
-  } );
+const {q = ''} = queryString.parse(location.search);
+
+
+  const heroesFiltered = heroes;
+  const navigate = useNavigate(); // Obtener la función de navegación
+
+  const [formValues, handleInputChange] = useForm({
+    searchText: q
+  });
   const { searchText } = formValues;
 
-  const handleSearch = (e) =>{
+  const handleSearch = (e) => {
     e.preventDefault();
-console.log( searchText );
-  }
+    navigate(`?query=${searchText}`); // Navegar a la ruta con el query string
+  };
 
   return (
     <div>
-      <h1> Search Screen </h1>
+      <h1>Search Screen</h1>
       <hr />
       <div className="row">
         <div className="col-5">
-          <h4> Search Form</h4>
+          <h4>Search Form</h4>
           <hr />
-          <form onSubmit = { handleSearch }>
+          <form onSubmit={handleSearch}>
             <input
               type="text"
               placeholder="Find your hero"
               className="form-control"
               name="searchText"
-              autoComplete= "off"
-              value={ searchText }
-              onChange= { handleInputChange }
+              autoComplete="off"
+              value={searchText}
+              onChange={handleInputChange}
             />
             <button
               type="submit"
@@ -44,13 +52,10 @@ console.log( searchText );
           </form>
         </div>
         <div className="col-7">
-          <h4>Results </h4>
+          <h4>Results</h4>
           <hr />
           {heroesFiltered.map((hero) => (
-            <HeroCard 
-            key={hero.id}
-            {...hero}
-            />
+            <HeroCard key={hero.id} {...hero} />
           ))}
         </div>
       </div>
