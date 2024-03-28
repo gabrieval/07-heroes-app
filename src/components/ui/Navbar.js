80ee73dee30c16c05b46799d97cb3393ddd45202
gabrieval <gabriela.valenzuela.rrii@gmail.com>
2024-03-28 11:52:28 -0300

    Login de un usuario

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,8 +1,14 @@
+import { useContext } from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import { AuthContext } from '../../auth/AuthContext';
 
 
 
 export const Navbar = () => {
+    const { user: {name} } =  useContext (AuthContext);
+    console.log(name);
+
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             
@@ -44,10 +50,10 @@ export const Navbar = () => {
 
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
                 <ul className="navbar-nav ml-auto">
-                    <p className="nav-item nav-link text-info">
+                    <span className="nav-item nav-link text-info">
 
-                        Susana
-                    </p>
+                        {name}
+                    </span>
                     <NavLink 
                         className="nav-item nav-link" 
                         to="/login"
